8226a1054383267d07b19894eb5e7c72aae7643e
Dan Hartman <dphart9@gmail.com>
2015-11-19 13:37:50 -0500

    stuff

---
+++ src/daScripts/SendEmail.java
@@ -0,0 +1,25 @@
+package daScripts;
+
+import org.openqa.selenium.WebDriver;
+
+public class SendEmail  {
+	public static void run(WebDriver driver) throws InterruptedException{
+//		try{
+//		String icon = "email-icon";
+//		String emailTo = "edit-email-to";
+//		String nameField = "edit-senders-name";
+//		String sendButton = "edit-send";
+//		String returnButton = "//*[@id=\"validate\"]/a";
+//		
+//		driver.findElement(By.id(icon)).click();
+//		driver.findElement(By.id(emailTo)).sendKeys("da.test.email02@gmail.com");
+//		driver.findElement(By.id(nameField)).sendKeys("Test");
+//		driver.findElement(By.id(sendButton)).click();
+//		
+//		driver.findElement(By.xpath(returnButton)).click();
+//		}
+//		catch(Exception e){
+//			
+//		}
+	}	
+}
